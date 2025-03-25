0c1c501da51dbdd020b66b35193076a2dc816cec
najuwamr <najuwamaulidarhm@gmail.com>
2025-03-25 22:29:17 +0700

    init commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const block = document.querySelector(".block-wrapper");
+const button = document.getElementById("mode");
+const body = document.querySelector("body"); 
+const picture = document.getElementById("my-pics")
+
+
+console.log(button, body);
+
+button.addEventListener("click", function() {
+    console.log("Tombol diklik!"); 
+    body.classList.toggle('dark-mode');
+});
+
+picture.addEventListener("click", function(){
+    if (this.src.includes("pictures/my-pic1.png")) {
+        this.src = "pictures/my-pic2.png";
+    } else {
+        this.src = "pictures/my-pic1.png";
+    }
+})
